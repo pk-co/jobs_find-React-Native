e003bf1da657f3e0f4e46a9b63965b26d8d8a81f
pk-co <prasacode@gmail.com>
2023-04-12 21:50:33 +0530

    Add The all Components

---
+++ components/home/nearby/NearByJobs.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { View,Text } from "react-native";
+
+import styles from './nearbyjobs.style';
+
+const NearByJobs = () => {
+    return (
+        <View>
+            <Text>Nearbyjobs</Text>
+        </View>
+    )
+}
+
+
+export default NearByJobs;
