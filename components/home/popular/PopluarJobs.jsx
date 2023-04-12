8a2855d452b5b043150897c8ef989201b6eb9a72
pk-co <prasacode@gmail.com>
2023-04-12 21:53:47 +0530

    popularjob.jsx file added

---
+++ components/home/popular/PopluarJobs.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { View,Text } from "react-native";
+
+import styles from './popularjobs.style';
+
+const PopularJobs = () => {
+    return (
+        <View>
+            <Text>PopularJobs</Text>
+        </View>
+    )
+}
